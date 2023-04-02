23cb1863cb70ca3c0611b00469c21f83c2e1e2c3
Darina <darina_p@rambler.ru>
2023-04-02 19:04:07 +0500

    Заменила на файл из firefox

--- test_add_group.py
+++ test_add_group.py
@@ -7,15 +7,14 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
+class AddNewGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-
-    def test_add_group(self):
+    
+    def test_add_new_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").clear()
@@ -25,13 +24,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new group")
+        wd.find_element_by_name("group_name").send_keys("New2")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("121233")
+        wd.find_element_by_name("group_header").send_keys("dfggdg")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("112234")
+        wd.find_element_by_name("group_footer").send_keys("dfdfss")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
@@ -45,7 +44,6 @@ class TestAddGroup(unittest.TestCase):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-
     
     def tearDown(self):
         self.wd.quit()
