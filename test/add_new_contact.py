e8bad437f06f2a70c01ff0db02e4b672c1dc1db1
Darina <darina_p@rambler.ru>
2023-04-09 19:27:50 +0500

    Выделен класс для работы с контактами

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-from contact import Contact
-from application2 import Application2
+from model.contact import Contact
+from fixture.application import Application
 import pytest
 
 @pytest.fixture
 def app(request):
-    fixture = Application2()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 def test_add_new_contact(app):
-    app.login("admin", "secret")
-    app.create_new_contact(Contact("Masha", "Ivanovna", "Ivanova", "IP Maria", "Main", "Mary27", "Russian Federation",
+    app.session.login("admin", "secret")
+    app.contact.create_new(Contact("Masha", "Ivanovna", "Ivanova", "IP Maria", "Main", "Mary27", "Russian Federation",
                             "738412", "89092154315", "745640", "222351", "maria27@mail.ru", "maria28@mail.ru",
                             "mariaiv27@mail.ru", "www.masha27.ru", "10", "August", "1999", "12", "June", "2010",
                             "dddd", "dfgs", "12345"))
-    app.logout()
+    app.session.logout()
